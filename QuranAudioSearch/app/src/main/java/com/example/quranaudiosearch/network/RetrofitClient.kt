d01881f18f954699dfab46de8fb384b22c444de7
auzieuji <dhiaauziecantika0212@gmail.com>
2026-07-24 07:27:58 +0700

    Update Config

--- QuranAudioSearch/app/src/main/java/com/example/quranaudiosearch/network/RetrofitClient.kt
+++ QuranAudioSearch/app/src/main/java/com/example/quranaudiosearch/network/RetrofitClient.kt
@@ -6,7 +6,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 object RetrofitClient {
 
     private const val BASE_URL =
-        "http://192.168.4.1:8000/"
+        "http://192.168.x.x:8000/"
 
     val apiService: ApiService by lazy {
 
